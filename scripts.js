c4857ed1a25dcfec633ab6ef769087e77dd2ae9a
Sina <sina@thewolpers.com>
2022-10-26 16:04:24 -0700

    Update format and styling

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
-let gridSize = 9;
+let gridSize = 16;
 
 function drawCanvas() {
     let canvas = document.getElementById('canvas');
     for(i = 0; i < gridSize ** 2; i++) {
 		canvas.style.gridTemplateColumns = 'repeat( ' + gridSize + ', 1fr)';
         let box = document.createElement('div');
-        box.textContent = 'B' + i;
+        //box.textContent = 'B' + i;
         box.className = 'box';
         box.setAttribute('id', i);
         box.addEventListener('click', changeColor);
         canvas.appendChild(box); 
     }
 }
+
 drawCanvas();
 
 function changeColor(event) {
-    console.log(event.target.id);
     let boxId = event.target.id;
     let tempBox = document.getElementById(boxId);
-    tempBox.style.backgroundColor = 'red';
+    tempBox.style.backgroundColor = '#000000';
 }
