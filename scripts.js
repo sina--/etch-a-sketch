ac422dfb373dc86342b01af3209a883676151a02
Sina <sina@thewolpers.com>
2022-10-26 14:01:29 -0700

    Update canvas grid size to be determined by variable

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
-function etchasketch() {
-    let board = document.getElementById('board');
-    for(i = 0; i < 10; i++) {
+let gridSize = 9;
+
+function drawCanvas() {
+    let canvas = document.getElementById('canvas');
+    for(i = 0; i < gridSize ** 2; i++) {
+		canvas.style.gridTemplateColumns = 'repeat( ' + gridSize + ', 1fr)';
         let box = document.createElement('div');
         box.textContent = 'B' + i;
         box.className = 'box';
         box.setAttribute('id', i);
         box.addEventListener('click', changeColor);
-        board.appendChild(box); 
+        canvas.appendChild(box); 
     }
 }
-etchasketch();
+drawCanvas();
 
 function changeColor(event) {
     console.log(event.target.id);
